99f2b3d6dd72e6278fd06afc49f10c1715c35053
JoelsTI <74766789+JoelsTI@users.noreply.github.com>
2021-06-30 16:20:48 +0200

    fix matrix

--- Project/Libraries/Matrix.hpp
+++ Project/Libraries/Matrix.hpp
@@ -89,6 +89,13 @@ public:
     }
 };
 
+/// \brief
+/// SPI Bus Implementation
+/// \details
+/// The constructor puts all the pins into the bus.
+/// the write is used to write clock input.
+/// the data is used to send over data.
+/// the cs pin is the chip select pin.
 class bus{
 protected:
    friend class writeTransaction;
@@ -102,6 +109,10 @@ public:
     cs ( cs )
     {}
 	
+/// \brief
+/// sets pins to output
+/// \details
+///	sets Ht1632C Led Matrix pins to output and flushes these pins.
     void set_output(){
         write.direction_set_output();
         data.direction_set_output();
@@ -112,6 +123,11 @@ public:
     }
 };
 
+/// \brief
+/// SPI Transaction
+/// \details
+/// This class takes the spi bus and creates a transaction from it.
+/// The CS pin starts off high to mark the beginning and ends low to mark the ending.
 class writeTransaction{
 protected:
     bus &b;
@@ -129,7 +145,13 @@ public:
         b.set_output();
         cs.write(0);
     }
-	
+    
+/// \brief
+/// writes data to the bus
+/// \details
+///	This is a function that sends data to the receiving chip.
+/// The byte_length is an indication of how many bytes are sent.
+/// a stands for the data to send.	
     void writeData(uint8_t byte_length, uint16_t a){
         for (uint16_t bit = 1<<(byte_length-1); bit; bit >>= 1) {
             write.write(0);
@@ -145,6 +167,12 @@ public:
     }
 };
 
+/// \brief
+/// Matrix HT1632C
+/// \details
+/// This is the self-written library for my IPASS project. 
+/// The LED-matrix I'm using has a length of 16 pixels and a width of 24 pixels.
+/// It is controlled by the HT1632 chip. This chip uses a SPI bus.
 class HT1632C : public writeTransaction{
 protected:
 	bus &b;
@@ -162,12 +190,27 @@ public:
 		data(b.data),
 		cs(b.cs)
 		{}
-		
+
+/// \brief
+/// send command to LED matrix
+/// \details
+///	This function creates a temporary transaction of the spi bus.
+/// It is used to send commands to the HT1632C chip.	
+/// The standard bit given is to enable the System, which must be on at all times to ensure that changes are actually being made.		
 void cmnd(uint8_t cmnd = 0x01){
 	writeTransaction command(b);
 	command.writeData(12, (((uint16_t)HT1632C_DATA_LEN << 8) | cmnd) << 1 );
 }
 
+/// \brief
+/// Initialize LED Matrix
+/// \details
+/// Using the cmnd function above, it turns on all the necessary commands for the chip to work.
+/// First of all the system oscillator is turned on. Because the standard bit given is already the byte needed, no byte is given.
+/// Secondly the LED duty cycle generator is turned on. This allows the LEDS to turn on and off.
+/// Thirdly the Blinking effect is turned off, so that we get a still image.
+/// Fourthly the on-chip RC oscillator is turned on.
+/// Lastly the N-MOS open drain output and 16 COM option is selected.
 void initialize(){
 	cmnd();
 	cmnd(HT1632C_CMD_LEDON);
@@ -176,6 +219,13 @@ void initialize(){
 	cmnd(HT1632C_CMD_COMS01);	
 } 
 
+/// \brief
+/// Clears the LED Matrix
+/// \details
+/// This function clears all the LEDS on the LED Matrix.
+/// First off the operation write is called upon with the first writeData command.
+/// Second off 0 is being written to all 7 of the memory adresses.
+/// Lastly it writes 0 to all the ROWS and COMS.
 void clear(){
 	writeTransaction command(b);
 	command.writeData(3, 0x05);
@@ -185,11 +235,22 @@ void clear(){
 	}
 }
 
+/// \brief
+/// Sets a Pixel on the LED Matrix
+/// \details
+/// This function sets a pixel on the LED matrix using hwlib::xy.
+/// It creates an x location from 0 to 15 and an y location from 0 to 23.
 void setPixel(hwlib::xy xy) {
 		if((xy.x < 0) || (xy.x >= 16) || (xy.y < 0) || (xy.y >= 24)) return;
 		array[xy.y] |= 0x8000 >> xy.x;
 }
 
+/// \brief
+/// Flushes the data
+/// \details
+/// All the data in the buffer gets transferred to the permanent memory.
+/// Once this function is called upon changes actually happen on the LED matrix.
+/// This is because the flush function allows all the commands to be processed.
 void flush(){
 	writeTransaction command(b);
 	command.writeData(3, 0x05);
